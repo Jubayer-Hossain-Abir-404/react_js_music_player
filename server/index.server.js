8579b6afad4f4d0ee7c7abc0ac70c77cb4773319
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-02-05 23:46:52 +0600

    Update

--- server/index.server.js
+++ server/index.server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
+const { appData } = require("./mock");
 const app = express();
 const PORT = 9000;
 
@@ -14,12 +16,51 @@ function getDirectories(type){
     return fs.readdirSync(`./music/${type}`);
 }
 
+function getSongDetails(type, dir){
+    try{
+        const path = `music/${type}/${dir}/media`;
+        const audioData = fs.readdirSync(path);
+        const audioInfo = fs.readFileSync(`./music/${type}/${dir}/info.json`);
+        const parseData = JSON.parse(audioInfo);
+        return {
+            audioFile: `${path}/${audioData[0]}`,
+            avatar: `${path}/${audioData[1]}`,
+            ...parseData
+        }
+    }catch(error){
+        return false;
+    }
+    
+}
+
+app.use(cors());
+app.use('/music',express.static('music'));
+
 app.get(`/song`, (req, res) => {
 
+    const songData = {};
+
     for(let type in __AUDIO_TYPE__){
-        console.log(getDirectories(__AUDIO_TYPE__[type]));
+        const directoryItems = getDirectories(__AUDIO_TYPE__[type]);
+        directoryItems.forEach((item) => {
+            const audioData = getSongDetails(__AUDIO_TYPE__[type], item);
+            if(audioData){
+                // console.log({ audioData });
+                if(__AUDIO_TYPE__[type] in songData){
+                    songData[__AUDIO_TYPE__[type]].push(audioData);
+                }else{
+                    songData[__AUDIO_TYPE__[type]] = [audioData];
+                }
+            }
+            
+        })
     }
-    res.send("Hello from server");
+    // res.send("Hello from server");
+    // console.log({songData});
+
+    appData['freelicense'] = songData
+
+    res.status(200).json({ appData });
 });
 
 app.listen(PORT, () =>{
